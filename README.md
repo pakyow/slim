118a00f406fdea6b7dd54ac6cc805c0ec517d9fb
Dan McGuire <dan@metabahn.com>
2014-12-23 12:25:55 -0600

    adds pakyow specific shortcuts

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Installation
 
-    gem install pakyow-slim
+Place inside your Gemfile
+
+    gem 'pakyow-slim'
 
 Inside app.rb
 
@@ -9,3 +11,17 @@ Inside app.rb
 Restart your app server and change your view extentions from .html to .slim
 
 For more info on slim templating language visit [slim-lang.com](http://slim-lang.com/)
+
+### Pakyow Specific shortcuts:
+
+    .klass@@skope
+
+translates to
+
+    <div class='klass' data-scope='skope'></div>
+
+    .klass@prop
+
+translates to
+
+    <div class='klass' data-prop='prop'></div>
--- lib/pakyow-slim.rb
+++ lib/pakyow-slim.rb
@@ -3,12 +3,27 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require 'slim'
 
+
+module SetSlimOptions
+      Slim::Engine.set_options(
+        shortcut:
+          { '@@' => { tag: 'div', attr: 'data-scope' },
+            '@' => { tag: 'div', attr: 'data-prop' },
+            '#' => { tag: 'div', attr: 'id' },
+            '.' => { tag: 'div', attr: 'class' }
+          }
+      )
+end
+
 Pakyow::App.processor :slim do |content|
-  temp = Tempfile.new('foo')
-  temp.write(content)
-  temp.rewind
-  processed_content = Slim::Template.new(temp.path).render
-  temp.close
-  temp.unlink
+  begin
+    temp = Tempfile.new('foo')
+    temp.write(content)
+    temp.rewind
+    processed_content = Slim::Template.new(temp.path).render
+  ensure
+    temp.close
+    temp.unlink
+  end
   processed_content
 end
